f1e37c03180cb1f58ed2dfd04255f412142563f0
ahmadhha@gmail.com <ahmadhha@gmail.com>
2023-02-12 22:41:14 -0500

    revised javascript code

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -2,11 +2,11 @@
 const samples =
   "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Fetch the JSON data and console log it
-d3.json(samples).then(function(data) {
+// Fetch the JSON data
+d3.json(samples).then(function (data) {
 
-// Initial Plots and Data key
-var init_id = 0;
+  // Initial Plots and Data key
+  let init_id = 0;
 
   // Display the default plot
   function init_bar() {
@@ -40,8 +40,6 @@ var init_id = 0;
       width: 800,
     };
 
-    console.log(plotdata);
-
     Plotly.newPlot("bar", plotdata, layout);
   }
 
@@ -70,34 +68,28 @@ var init_id = 0;
       width: 1100,
     };
 
-    console.log(plotdata);
-
     Plotly.newPlot("bubble", plotdata, layout);
   }
 
   // Function to display and update metadata
   function metadata_val(key) {
     d3.select(".panel-body").html(
-        "Key:" + `${data.metadata.map((i, j) => j)[key]}` + "<br><br>" +
-        "id:" + `${data.metadata[key].id}` + "<br>" +
-        "ethnicity:" + `${data.metadata[key].ethnicity}` + "<br>" +
-        "gender:" + `${data.metadata[key].gender}` + "<br>" +
-        "age:" + `${data.metadata[key].age}` + "<br>" +
-        "location:" + `${data.metadata[key].location}` + "<br>" +
-        "bbtype:" + `${data.metadata[key].bbtype}` + "<br>" +
-        "wfreq:" + `${data.metadata[key].wfreq}` + "<br>"
-        )};
+      "Key:" + `${data.metadata.map((i, j) => j)[key]}` + "<br><br>" +
+      "id:" + `${data.metadata[key].id}` + "<br>" +
+      "ethnicity:" + `${data.metadata[key].ethnicity}` + "<br>" +
+      "gender:" + `${data.metadata[key].gender}` + "<br>" +
+      "age:" + `${data.metadata[key].age}` + "<br>" +
+      "location:" + `${data.metadata[key].location}` + "<br>" +
+      "bbtype:" + `${data.metadata[key].bbtype}` + "<br>" +
+      "wfreq:" + `${data.metadata[key].wfreq}` + "<br>"
+    )
+  };
 
 
   // Populate options
   let options = "";
   for (let i = 0; i < data.metadata.length; i++) {
-    options +=
-      "<option value=" +
-      `${data.metadata[i].id}` +
-      ">" +
-      `${data.metadata[i].id}` +
-      "</option> <br>";
+    options += "<option value=" + `${data.metadata[i].id}` + ">" + `${data.metadata[i].id}` + "</option> <br>";
   }
 
   // Declare options list
@@ -110,7 +102,6 @@ var init_id = 0;
   function optionChanged() {
     let dropdownMenu = d3.select("#selDataset");
     let new_id = data.names.indexOf(dropdownMenu.property("value"));
-    console.log(new_id);
 
     // Call function to update the chart and metadata
     updatePlotly(new_id);
@@ -118,27 +109,28 @@ var init_id = 0;
 
   // Update the restyled plot's values and metadata
   function updatePlotly(new_id) {
+    // Update bar chart
     Plotly.restyle("bar", "x", [data.samples[new_id].sample_values.slice(0, 10)]);
     Plotly.restyle("bar", "y", [data.samples[new_id].otu_ids.slice(0, 10).map(item => "OTU " + item.toString())]);
     Plotly.restyle("bar", "text", [data.samples[new_id].otu_labels.slice(0, 10)]);
-    console.log(new_id);
 
+    // Update bubble chart
     Plotly.restyle("bubble", "x", [data.samples[new_id].otu_ids]);
     Plotly.restyle("bubble", "y", [data.samples[new_id].sample_values]);
     Plotly.restyle("bubble", "text", [data.samples[new_id].otu_labels]);
     Plotly.restyle("bubble", "marker", [{
-            size: data.samples[new_id].sample_values,
-            color: data.samples[new_id].otu_ids,
-          }]);
-    
+      size: data.samples[new_id].sample_values,
+      color: data.samples[new_id].otu_ids,
+    }]);
 
+    // Update Metadata and Key-Value pair
     metadata_val(new_id);
 
-    }
+  }
 
-    //Call initial plots and data
-    init_bar();
-    init_bubble();
-    metadata_val(init_id);
+  // Call initial plots and data
+  init_bar();
+  init_bubble();
+  metadata_val(init_id);
 
 });
